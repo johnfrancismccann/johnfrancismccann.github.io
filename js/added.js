3f9e12f42e62ca21671a13c94dcae9590c229fcc
John McCann <john.francis.mccann@gmail.com>
2017-05-27 16:35:47 -0500

    when modal is opened, show that modal's hash in url bar. enable modal to be directly linked to.

--- js/added.js
+++ js/added.js
@@ -7,5 +7,23 @@
     });
 
     webshims.polyfill('forms es5');
+    
+    
+    // when page is loaded, if url points to hash, open the corresponding modal
+    $(window.location.hash).modal('show');
+    // show modal's hash in url bar if user opens a modal
+    $('a[data-toggle="modal"]').click(function(){
+      window.location.hash = $(this).attr('href');
+    });
+
+    // remove modal's hash in url bar if user closes the modal
+    function revertToOriginalURL() {
+        var original = window.location.href.substr(0, window.location.href.indexOf('#'))
+        history.replaceState({}, document.title, original);
+    }
+    $('.modal').on('hidden.bs.modal', function () {
+        revertToOriginalURL();
+    });
+
 })(jQuery);
 
